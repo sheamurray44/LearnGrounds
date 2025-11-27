b1b47b90a2c86dcb3fdf1b6694cd032e7935bb0f
sheamurray44 <148862489+sheamurray44@users.noreply.github.com>
2025-11-27 16:18:35 +0000

    update

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -4,7 +4,7 @@ using TMPro;
 public class InventoryUI : MonoBehaviour
 {
     private TextMeshProUGUI itemText;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         itemText = GetComponent<TextMeshProUGUI>();
