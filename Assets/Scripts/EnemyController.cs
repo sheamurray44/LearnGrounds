96951a2eff875a1192e37cfd606547023f27086c
sheamurray44 <148862489+sheamurray44@users.noreply.github.com>
2025-12-11 14:45:18 +0000

    stuff

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -21,7 +21,19 @@ public class EnemyController : MonoBehaviour
         if (distance <= lookRadius) 
         {
             agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+            }
         }
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 
     private void OnDrawGizmosSelected()
